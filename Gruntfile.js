6daac011f154074475789aed401f124a34b4899d
albert.xu <albert.xu@virtualu>
2013-12-31 11:47:25 +0800

    plato

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,10 +15,18 @@ module.exports = function(grunt) {
         'tests/specs/**/*.js'
       ]
     },
+    plato: {
+      all: {
+        files: {
+          'platoreport': ['scripts/**/*.js', 'tests/**/*.js'],
+        }
+      },
+    }
   });
 
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-plato');
 
   grunt.registerTask('default', [ ]);
   grunt.registerTask('test', [ 'jshint', 'mochaTest' ]);
