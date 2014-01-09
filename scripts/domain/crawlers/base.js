15c9814027c2898c1ea8b37c6f24f226cca02cd6
albert.xu <albert.xu@virtualu>
2014-01-09 18:37:14 +0800

    use Jake on OpenShift as a backup

--- scripts/domain/crawlers/base.js
+++ scripts/domain/crawlers/base.js
@@ -4,13 +4,64 @@ var _ = require('lodash'),
 
 var winston = require('../../logger');
 
-function Crawler (argument) {
+function Crawler(argument) {
   return this;
 }
 
 Crawler.prototype.name = "Crawler";
 
-Crawler.prototype.get = function(url, options) {
+
+function _get(url, options) {
+  var d = q.defer();
+
+  request.get(url, options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      d.resolve(body);
+    } else if (error.code === 'ETIMEDOUT' || error.code === 'ESOCKETTIMEDOUT') {
+      d.resolve('timeout');
+    } else {
+      console.log(error, response, body);
+      d.reject(error);
+    }
+  });
+
+  return d.promise;
+}
+
+function _get_openshift(url, options) {
+  var d = q.defer();
+
+  var u = 'http://jake-hbrls.rhcloud.com/crawler?url=' + encodeURIComponent(url);
+
+  request.get(u, options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      d.resolve(body);
+    } else if (error.code === 'ETIMEDOUT' || error.code === 'ESOCKETTIMEDOUT') {
+      d.resolve('timeout');
+    } else {
+      console.log(error, response, body);
+      // FIXME: need better error controlling
+      d.reject('Jake on OpenShift failed');
+      // if (response.statusCode === 500) {
+      //   if (body === 'timeout') {
+      //     d.resolve('timeout');
+      //   } else {
+      //     d.reject(body);
+      //   }
+      // } else {
+      //   console.log(error, response, body);
+      //   d.reject(error);
+      // }
+    }
+  });
+
+  return d.promise;
+}
+
+
+Crawler.prototype.get = function (url, options) {
+  // url = "http://www.facebook.com";
+
   var d = q.defer();
 
   var _default_options = {
@@ -21,16 +72,25 @@ Crawler.prototype.get = function(url, options) {
     _.extend(_default_options, options);
   }
 
-  request.get(url, _default_options, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      d.resolve(body);
-    } else if (error.code === 'ETIMEDOUT') {
-      d.reject('timeout connecting to ' + url);
-    } else {
-      console.log(error, response, body);
-      d.reject(error);
-    }
-  });
+  _get(url, _default_options)
+    .then(function (body) {
+      if (body === 'timeout') {
+        return _get_openshift(url, _default_options);
+      } else {
+        d.resolve(body);
+      }
+    })
+    .then(function (body) {
+      if (body === 'timeout') {
+        d.reject('timeout connecting to ' + url);
+      } else {
+        d.resolve(body);
+      }
+    })
+    .fail(function (EorW) {
+      d.reject(EorW);
+    })
+    .done();
 
   return d.promise;
 };
